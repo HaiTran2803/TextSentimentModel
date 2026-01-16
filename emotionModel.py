939dded1bd231f3b6b876092b1b690252be7b625
Hai Tran <93782316+HaiTran2803@users.noreply.github.com>
2026-01-16 13:17:31 +0700

    Update emotionModel.py

--- emotionModel.py
+++ emotionModel.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 import pandas as pd
-# from underthesea import word_tokenize
 from datasets import Dataset
 from sklearn.preprocessing import LabelEncoder
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
@@ -12,7 +11,6 @@ from transformers import TrainingArguments, Trainer
 dataframe = pd.read_csv("vietnamese_emotion_dataset.csv",nrows=45)
 subset = dataframe.iloc[:45]
 subset_selected_filtered = subset[["text","labels"]].copy()
-# print(subset_selected_filtered)
 
 ####### take the unique labels
 type = sorted(np.unique(subset_selected_filtered["labels"]))
@@ -34,11 +32,9 @@ for col in ['labels']:
 ###### preprocessing
 def preprocessing(text):
     text = text.lower().strip()
-    # text = word_tokenize(text, format="text")
     return text
 
 subset_selected_filtered['text'] = subset_selected_filtered['text'].apply(preprocessing)
-# print(subset_selected_filtered)
 
 ####### model
 MODEL_NAME = "vinai/phobert-base"
@@ -53,27 +49,26 @@ def tokenize(batch):
         max_length=128,
     )
 dataset = Dataset.from_pandas(subset_selected_filtered)
-# dataset = dataset.rename_column("label", "labels")
 dataset = dataset.train_test_split(test_size=0.1)
 dataset = dataset.map(tokenize, batched=True)
 dataset.set_format(type="torch",columns=["input_ids", "attention_mask", "labels"])
 
 ####### train model
 training_args = TrainingArguments(
-    output_dir="./phobert_sentiment", #output in directory
-    eval_strategy="epoch", # Đánh giá theo: epoch
-    save_strategy="epoch", # khi nào lưu checkpoint: epoch
-    learning_rate= 3e-5, # between 1e-5 to 5e-5 -> ~ 2 is good
-    per_device_train_batch_size= 4, # 4-16, 8gb VRam -> 4 is good
-    per_device_eval_batch_size= 4, # should 4 = train_batch_size
-    gradient_accumulation_steps= 4, # giả lập batch lớn hơn mà không tăng Vram
-    num_train_epochs= 4, # số lần học toàn bộ dataset
-    weight_decay=0.01, # regularization - chống overfitting -> 0.01 is good for transformers
-    logging_dir="./logs", # lưu log training with(loss,learning rate, Mertric
-    fp16=True, # train with 16 bit -> giảm 50% VRam, tăng tốc (chỉ dùng khi có GPU NVIDIA or support RTX series)
-    load_best_model_at_end=True, # sau khi train xong thì tự load checkpoint tốt nhất (evaluation_strategy = save_strategy = "epoch")
-    metric_for_best_model="eval_loss", # phần chạy ngầm nếu không chỉnh sửa
-    greater_is_better= False, # phần chạy ngầm
+    output_dir="./phobert_sentiment",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate= 3e-5,
+    per_device_train_batch_size= 4,
+    per_device_eval_batch_size= 4,
+    gradient_accumulation_steps= 4,
+    num_train_epochs= 4, 
+    weight_decay=0.01,
+    logging_dir="./logs",
+    fp16=True,
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    greater_is_better= False,
 )
 
 def compute_metrics(eval_pred):
